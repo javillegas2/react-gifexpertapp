c43b945916fcd4dff0541ba6fcf178ed4b5dd61f
javillegas2 <javillegas162@gmail.com>
2020-11-24 22:38:44 -0500

    html actualizado 2

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@
 
 
 export const getGifs = async (category) => {
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=6kFf2ganNheEaBIhCAjGB0kXaoXuDcfv`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=6kFf2ganNheEaBIhCAjGB0kXaoXuDcfv`;
     const resp = await fetch(url);
     const { data } = await resp.json();
 
